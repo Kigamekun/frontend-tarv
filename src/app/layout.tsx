e34bebacf74fa9eab5b1d73cda321e18574509e0
KIGAMEKUN <reksa.prayoga1012@gmail.com>
2024-05-18 20:48:35 +0700

    feat(add config)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,10 +17,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='scroll-smooth'>
+
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
       <body className={inter.className}>
-      <ReduxProvider>
-        {children}
-      </ReduxProvider>
+        <ReduxProvider>
+          {children}
+        </ReduxProvider>
       </body>
     </html>
   )
