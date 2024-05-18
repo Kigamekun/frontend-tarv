2b9b2ced8d5b5c53ee6f110c1e57ba77403ad243
tgrsnts <tegarsantoso72@gmail.com>
2024-05-18 14:41:01 +0700

    memindahkan button dan harga ke bawah

--- src/app/fruits/page.tsx
+++ src/app/fruits/page.tsx
@@ -195,7 +195,7 @@ export default function Home() {
                                 <>
                                     <div
                                         style={{ width: "18.5rem", border: "none !important" }}
-                                        className=" bg-white rounded-lg  :bg-gray-800 :border-gray-700"
+                                        className="flex flex-col bg-white rounded-lg  :bg-gray-800 :border-gray-700"
                                     >
                                         <center>
 
@@ -212,36 +212,33 @@ export default function Home() {
                                             </Link>
 
                                         </center>
-                                        <div className="p-5">
-                                            <Link href={`/home/${fruit.id}`}>
-                                                <h5 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 :text-white">
-                                                    {fruit.name}
-                                                </h5>
-                                            </Link>
-                                            <p className="text-[#a2a2a2] mb-1">Stok : {fruit.stock}</p>
-                                            <p className="mb-3 font-normal text-gray-700 :text-gray-400">
-                                                {fruit.description}
-                                            </p>
-                                            <div className="flex" style={{ justifyContent: "space-between" }}>
-                                                <div>
-                                                    <span>
-                                                        <h5 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 :text-white">
-                                                            Rp. {fruit.price} <span className="text-sm text-gray-500">/ Kg</span>
-                                                        </h5>
-                                                    </span>
-                                                </div>
-                                                <div>
-                                                    <button
-                                                        type="button" onClick={() => handleAddToCart(fruit)}
-                                                        className="text-white bg-[#DC1F26] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-                                                    >
-                                                        +
-                                                    </button>
-                                                </div>
+                                        <Link href={`/home/${fruit.id}`}>
+                                            <h5 className="px-5 mt-5 mb-1 text-2xl font-bold tracking-tight text-gray-900 :text-white">
+                                                {fruit.name}
+                                            </h5>
+                                        </Link>
+                                        <p className="px-5 text-[#a2a2a2] mb-1">Stok : {fruit.stock}</p>
+                                        <p className="px-5 mb-3 font-normal text-gray-700 :text-gray-400">
+                                            {fruit.description}
+                                        </p>
+                                        <div className="flex mt-auto px-5" style={{ justifyContent: "space-between" }}>
+                                            <div>
+                                                <span>
+                                                    <h5 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 :text-white">
+                                                        Rp. {fruit.price} <span className="text-sm text-gray-500">/ Kg</span>
+                                                    </h5>
+                                                </span>
+                                            </div>
+                                            <div>
+                                                <button
+                                                    type="button" onClick={() => handleAddToCart(fruit)}
+                                                    className="text-white bg-[#DC1F26] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                                                >
+                                                    +
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
-
                                 </>
                             )) : <div className="text-center">Tidak Ada data</div>}
 
